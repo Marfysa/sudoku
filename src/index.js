76eee9542445549c7aa8296ac61da998f49c8f2c
Maria Kurakevich <marfysa14@gmail.com>
2021-08-20 14:30:20 +0300

    add sol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  //
+  let columnWithZeroIdx = [];
+  let columnCount = matrix[0].length;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[i][j] === 0) {
+        for (let num = 1; num < 10; num++) {
+          if (variant(i, j, num, matrix)) {
+          matrix[i][j] = num;
+          if (solveSudoku(matrix)) {
+            return matrix;
+          } else {
+            matrix[i][j] = 0;
+          }
+        }
+      }
+      return false;
+    }
+  }
 }
+return matrix;
+}
+    function variant (i, j, num, arr) {
+      for (let a = 0; a < arr.length; a++) {
+        if (arr[i][a] === num) {
+          return false; 
+        }
+      }
+
+      for (let a = 0; a < arr.length; a++) {
+        if (arr[a][j] === num) {
+          return false;
+        }
+        
+      }
+    }
+
+  
